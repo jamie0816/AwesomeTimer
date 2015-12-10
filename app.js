e79d445b05fbb78855cdc5772df090c9252f6e8e
jamie0816 <jamie020816@gmail.com>
2015-12-09 15:48:09 -1000

    d
    d

--- app.js
+++ app.js
@@ -1,49 +1,21 @@
-window.setTimeout("Tick()", 1000);
-function Tick() {
-}
-function UpdateTimer() {
-var Seconds = TotalSeconds;
+var timer;
+var t;
+var beep = new Audio("beep.mp3");
 
-var Days = Math.floor(Seconds / 86400);
-Seconds -= Days * 86400;
+function startTimer() {
+	timer = prompt("Set Countdown");
+	timedCount();
+};
 
-var Hours = Math.floor(Seconds / 3600);
-Seconds -= Hours * (3600);
-
-var Minutes = Math.floor(Seconds / 60);
-Seconds -= Minutes * (60);
-
-
-var TimeStr = ((Days > 0) ? Days + " days " : "") + LeadingZero(Hours) + ":" + LeadingZero(Minutes) + ":" + LeadingZero(Seconds)
-
-
-Timer.innerHTML = TimeStr;
-}
-
-
-function LeadingZero(Time) {
-
-return (Time < 10) ? "0" + Time : + Time;
-
-}
-var Timer;
-var TotalSeconds;
-
-
-function CreateTimer(TimerID, Time) {
-Timer = document.getElementById(TimerID);
-TotalSeconds = Time;
-
-UpdateTimer()
-window.setTimeout("Tick()", 1000);
-}
-
-function Tick() {
-TotalSeconds -= 1;
-UpdateTimer()
-window.setTimeout("Tick()", 1000);
-}
-
-function UpdateTimer() {
-Timer.innerHTML = TotalSeconds;
-}
+function timedCount(){
+	document.getElementById('clock').innerHTML = timer;
+	if (timer === 0) {
+		beep.play();
+		document.getElementById('clock').innerHTML = "Times Up!";
+		//alert("Times Up!");
+	}
+	 else {
+	timer--;
+	t = setTimeout(function(){timedCount()},1000);
+	};
+};
